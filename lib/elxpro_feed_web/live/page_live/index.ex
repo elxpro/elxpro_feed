7481db0de51c23087a7c217934678b7766e1eb71
Gustavo <g.92oliveira@gmail.com>
2022-12-04 07:29:02 -0300

    split form to a function component

--- lib/elxpro_feed_web/live/page_live/index.ex
+++ lib/elxpro_feed_web/live/page_live/index.ex
@@ -9,13 +9,18 @@ defmodule ElxproFeedWeb.PageLive.Index do
         id: "123",
         avatar_url: "https://avatars.githubusercontent.com/u/12762300?v=4",
         author: "Gustavo Oliveira",
-        role: "Elixir Developer"
+        role: "Elixir Developer",
+        comments: [
+          id: "432",
+          content: "Pumpkin 123"
+        ]
       },
       %{
         id: "1234",
         avatar_url: "https://avatars.githubusercontent.com/u/12762300?v=4",
         author: "Joe Morris",
-        role: "Principal Engineer"
+        role: "Principal Engineer",
+        comments: []
       }
     ]
 
